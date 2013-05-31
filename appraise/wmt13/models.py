23133ff6e0bae38f1f52dff26888bc4ca16e542c
Christian Federmann <cfedermann@gmail.com>
2013-05-31 18:55:22 +0200

    Do not count "empty" users.

--- appraise/wmt13/models.py
+++ appraise/wmt13/models.py
@@ -181,12 +181,15 @@ class HIT(models.Model):
         Computes the HIT completion status for users of the given group.
         """
         combined = [0, 0, 0]
-        users = group.user_set.count()
+        users = 0
         for user in group.user_set.all():
             _user_status = cls.compute_status_for_user(user, language_pair)
             combined[0] = combined[0] + _user_status[0]
             combined[1] = combined[1] + _user_status[1]
             combined[2] = combined[2] + _user_status[2]
+            
+            if _user_status[0] > 0:
+                users = users + 1
         
         combined[1] = combined[1] / float(users)
         return combined

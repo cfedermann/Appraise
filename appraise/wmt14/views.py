c694a6e4d38be58dad0e26a33e66fce4c1bd415d
Christian Federmann <cfedermann@gmail.com>
2014-03-20 11:53:30 -0700

    Updated group/HIT requirements.

--- appraise/wmt14/views.py
+++ appraise/wmt14/views.py
@@ -758,7 +758,8 @@ def _compute_group_stats():
       'DCU-Prompsit-UA': 200, 'RWTH': 100, 'CIS/IMS': 100, 'Eu-Bridge': 200,
       'IIT-Bombay': 300, 'YSDA': 100, 'UU': 300, 'UA-Prompsit': 100,
       'IMS-TTT': 100, 'UFAL': 600, 'AFRL': 300, 'UEDIN': 1900,
-      'Stanford': 300, 'UB-Grial': 100
+      'Stanford': 300, 'UB-Grial': 100, 'QCRI': 200, 'LIMSI': 100,
+      'USAAR': 300, 'IPN-UPV': 200, 'CMU': 200,
     }
     
     for group in groups:
